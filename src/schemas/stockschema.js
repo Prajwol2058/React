5b00ed32d20f635dcb0ae5727c47fde0d8fbf73e
prajwol2058 <prajwol.bakhati@live.com>
2023-12-20 11:44:08 +0545

    added validation conditions

--- src/schemas/stockschema.js
+++ src/schemas/stockschema.js
@@ -3,8 +3,15 @@ import { object, string, number, date } from "yup";
 
 export const stockSchema = object({
   securityId: string().required("securityId compulsory"),
+  securityId: number()
+    .required("Securtiy Id chaahinxa!")
+    .positive("negative hunu bhayena hajur")
+    .integer("no decimals allowed"),
   securityName: string().required("securityname compulsory"),
   symbol: string().required("symbol required"),
+  symbol: string
+    .required("Symbol chaahinxa!")
+    .min(3, "3 letters hunai paryo ni!"),
   //   totalTradedQuantity: number().default(() => 0),
   //   lastTradedPrice: number().required().positive(),
 });
